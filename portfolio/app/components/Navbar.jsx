e3bd5d72a2adb4b4ce07e63fdfe5de17c3756b65
Raineetu <raineetu0070@gmail.com>
2025-06-07 21:37:40 +0545

    feat:added new content

--- portfolio/app/components/Navbar.jsx
+++ portfolio/app/components/Navbar.jsx
@@ -5,6 +5,9 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
+      <div className="fixed top-0 -z-10  translate-y-[-80%]">
+        <Image src={assets.header_bg_color} alt="" className="w-full" />
+      </div>
       <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ">
         <a href="#top">
           <Image
@@ -13,32 +16,58 @@ const Navbar = () => {
             className="w-28 cursor-pointer mr-14"
           />
         </a>
-        <ul className="hidden md:flex items-center gap-6 lg:gap-12 rounded-full px-12 py-3 ">
+        <ul className="hidden md:flex items-center gap-6 lg:gap-12 rounded-full px-12 py-3 bg-white">
           <li className="justify-between items-center ">
-            <a href="#top">Home</a>
+            <a className="font-Ovo" href="#top">
+              Home
+            </a>
           </li>
           <li>
-            <a href="#about">About Me</a>
+            <a className="font-Ovo " href="#about">
+              About Me
+            </a>
           </li>
           <li>
-            <a href="#services">Services</a>
+            <a className="font-Ovo" href="#services">
+              Services
+            </a>
           </li>
           <li>
-            <a href="#work">My Work</a>
+            <a className="font-Ovo" href="#work">
+              My Work
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact Me</a>
+            <a className="font-Ovo" href="#contact">
+              Contact Me
+            </a>
           </li>
         </ul>
 
-        <div>
+        <div className="flex items-center gap-4">
+          <button>
+            <Image
+              src={assets.moon_icon}
+              alt="icon"
+              className="w-5 cursor-pointer"
+            />
+          </button>
+
           <a
             href="#contact"
-            className="hidden md:flex items-center gap-3 px-8 py-2.5 border border-gray-500 rounded-full ml-4"
+            className="hidden md:flex items-center gap-3 px-8 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo"
           >
             Contact
             <Image src={assets.arrow_icon} alt="contact" className="w-3" />
           </a>
+
+          <button className="block md:hidden ml-3">
+            <Image
+              src={assets.menu_black}
+              alt="menu"
+              className="w-5 cursor-pointer"
+            />
+          </button>
         </div>
       </nav>
     </>

8dcb72754189c4c7c35a5f2b1502b683517bce89
Raineetu <raineetu0070@gmail.com>
2025-06-07 15:51:42 +0545

    chore:customize postcss.config with new colors and fonts

--- portfolio/app/components/Navbar.jsx
+++ portfolio/app/components/Navbar.jsx
@@ -5,12 +5,16 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <nav>
-        <a href="">
-          <Image src={assets.logo} className="w-28 cursor-pointer mr-14" />
+      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ">
+        <a href="#top">
+          <Image
+            src={assets.logo}
+            alt="logo"
+            className="w-28 cursor-pointer mr-14"
+          />
         </a>
-        <ul>
-          <li>
+        <ul className="hidden md:flex items-center gap-6 lg:gap-12 rounded-full px-12 py-3 ">
+          <li className="justify-between items-center ">
             <a href="#top">Home</a>
           </li>
           <li>
@@ -28,9 +32,12 @@ const Navbar = () => {
         </ul>
 
         <div>
-          <a href="#contact">
+          <a
+            href="#contact"
+            className="hidden md:flex items-center gap-3 px-8 py-2.5 border border-gray-500 rounded-full ml-4"
+          >
             Contact
-            <Image src={assets.arrow_icon} className="w-" />
+            <Image src={assets.arrow_icon} alt="contact" className="w-3" />
           </a>
         </div>
       </nav>
--- portfolio/postcss.config.mjs
+++ portfolio/postcss.config.mjs
@@ -1,5 +1,18 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  theme: {
+    extend: {
+      colors: {
+        lightHover: "#fcf4ff",
+        darkHover: "#2a004a",
+        darkTheme: "#11001F",
+      },
+      fontFamily: {
+        Outfit: ["Outfit", "sans-serif"],
+        Ovo: ["Ovo", "serif"],
+      },
+    },
+  },
 };
 
 export default config;

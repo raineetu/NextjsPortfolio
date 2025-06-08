ff4ad756aa5a57ad801066a48d64ec855b3cc3a2
Raineetu <raineetu0070@gmail.com>
2025-06-08 16:06:39 +0545

    feat:made a header component

--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,11 +1,13 @@
 "use client";
 
+import Header from "./components/headercomp/Header";
 import Navbar from "./components/navbarcomp/Navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   );
 }

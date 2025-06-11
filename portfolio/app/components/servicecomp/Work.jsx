78fd931058f44713a37822a7974cd7a95d5522b0
Raineetu <raineetu0070@gmail.com>
2025-06-11 21:29:18 +0545

    chore: added grid

--- portfolio/app/components/servicecomp/Work.jsx
+++ portfolio/app/components/servicecomp/Work.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import SectionHeader from "../commoncomp/SectionHeader";
+import { assets, workData } from "@/constants/assets";
+import Image from "next/image";
 
 const Work = () => {
   return (
@@ -12,6 +14,26 @@ const Work = () => {
         neque ipsam, beatae quas dolore sit nisi voluptates doloremque modi ab
         earum veritatis facilis, laboriosam, aut nihil soluta minima quia!"
       />
+      <div className="grid grid-cols-[repeat(auto-fit, minmax(200px, 1fr))] gap-5 my-10">
+        {workData.map((work, index) => (
+          <div
+            key={index}
+            style={{ backgroundImage: `url(${work.bgImage})` }}
+            className="w-full max-w-sm mx-auto  bg-no-repeat bg-cover bg-center group"
+          >
+            <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between group-hover:bottom-7 duration-300">
+              <div>
+                <h2>{work.title}</h2>
+                <p>{work.description}</p>
+              </div>
+
+              <div>
+                <Image src={assets.send_icon} alt="send_icon" className="w-5" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

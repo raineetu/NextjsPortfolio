720460b7334aaf5acaaac39ca690c2d119970f47
Raineetu <raineetu0070@gmail.com>
2025-06-13 19:51:10 +0545

    chore: sending mail using web3form

--- portfolio/app/components/contactcomp/Contact.jsx
+++ portfolio/app/components/contactcomp/Contact.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import ContactForm from "./ContactForm";
 
 const Contact = () => {
   return (
     <div
       id="contact"
-      //   className="w-full px-[12%] py-10 scroll-mt-20 text-center mb-2 font-Ovo bg-[url("/footer-bg-color.png") bg-no-repeat bg-center bg-[length:90%_auto]]"
-      //   style={{ backgroundImage: `url(${footer - bg - color.png})` }}
+      className="w-full px-[12%] py-10 scroll-mt-20 text-center mb-2 font-Ovo bg-[url('/footer-bg-color.png')] bg-no-repeat bg-center bg-[length:90%_auto]"
     >
-      <h4 className=" text-xl ">COnnect with me</h4>
+      <h4 className=" text-xl ">Connect with me</h4>
       <h2 className=" text-4xl ">Get in touch</h2>
       <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit magni
@@ -16,14 +16,7 @@ const Contact = () => {
         non. Asperiores, impedit.
       </p>
 
-      <form>
-        <div>
-          <input type="text" placeholder="Enter your name" required />
-          <input type="email" placeholder="Enter your email" required />
-        </div>
-        <textarea rows="6" placeholder="Enter your message" required></textarea>
-        <button type="submit">Submit now</button>
-      </form>
+      <ContactForm />
     </div>
   );
 };
---
+++ portfolio/app/components/contactcomp/ContactForm.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import { assets } from "@/constants/assets";
+import Image from "next/image";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const ContactForm = () => {
+  const [result, setResult] = useState("");
+  const [sending, setSending] = useState(false);
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setSending(true);
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "c5bb2c38-c23f-4d74-8a79-9ed41d49a9f1");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      toast.success("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      toast.error("Form Submission failed");
+      setResult(data.message);
+    }
+    setSending(false);
+  };
+  return (
+    <div>
+      <form className="max-w-2xl mx-auto  px-5" onSubmit={onSubmit}>
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-5 my-10 ">
+          <input
+            type="text"
+            placeholder="Enter your name"
+            required
+            name="name"
+            className="flex-1 p-3 border-[0.5] border-gray-400 rounded-lg outline-none bg-white"
+          />
+          <input
+            type="email"
+            placeholder="Enter your email"
+            required
+            name="email"
+            className="flex-1 p-3 border-[0.5] border-gray-400 rounded-lg outline-none bg-white"
+          />
+        </div>
+        <textarea
+          rows="6"
+          placeholder="Enter your message"
+          required
+          name="message"
+          className="w-full p-3 border-[0.5] border-gray-400 rounded-lg outline-none bg-white"
+        ></textarea>
+        <button
+          type="submit"
+          className="py-3 px-6 w-max flex items-center justify-center gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 cursor-pointer"
+        >
+          <ToastContainer position="top-center" autoClose={2000} />
+
+          {sending ? (
+            <span>Sending...</span>
+          ) : (
+            <>
+              Submit now
+              <Image
+                src={assets.right_arrow_white}
+                alt="right arrow"
+                className="w-4"
+              />
+            </>
+          )}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;

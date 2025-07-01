420bec8a52c33844c31d237ec90bdf5bd9b7f1da
Neetu Rai <raineetu0070@gmail.com>
2025-07-01 20:29:32 +0545

    fix: default set to dark

--- portfolio/app/context/ThemeContext.js
+++ portfolio/app/context/ThemeContext.js
@@ -3,25 +3,29 @@ import { createContext, useContext, useState, useEffect } from "react";
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState("dark"); // default is dark
 
   useEffect(() => {
     const savedTheme = localStorage.getItem("theme");
-    const preferredTheme = window.matchMedia(
+    const prefersDark = window.matchMedia(
       "(prefers-color-scheme: dark)"
     ).matches;
-    const initialTheme = savedTheme || (preferredTheme ? "dark" : "light");
+    const initialTheme = savedTheme || (prefersDark ? "dark" : "light");
+
     setTheme(initialTheme);
-    document.documentElement.classList.remove("light", "dark");
-    document.documentElement.classList.add(initialTheme);
+    updateDOMTheme(initialTheme);
   }, []);
+
+  const updateDOMTheme = (theme) => {
+    document.documentElement.classList.remove("light", "dark");
+    document.documentElement.classList.add(theme);
+  };
 
   const toggleTheme = () => {
     const newTheme = theme === "dark" ? "light" : "dark";
     setTheme(newTheme);
     localStorage.setItem("theme", newTheme);
-    document.documentElement.classList.remove("light", "dark");
-    document.documentElement.classList.add(newTheme);
+    updateDOMTheme(newTheme);
   };
 
   return (
